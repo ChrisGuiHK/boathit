d4c3fc32563a67ffea71f30c55534916f13e431a
ChrisGuiHK <200110701@stu.hit.edu.cn>
2023-06-28 19:36:51 +0800

    fix some bugs

--- utils/RandomSubsetSampler.py
+++ utils/RandomSubsetSampler.py
@@ -20,7 +20,7 @@ class RandomSubsetSampler(Sampler[int]):
         
         for _ in range(self._num_samples // 32):
             yield from self.indice[torch.randint(high=n, size=(32,), dtype=torch.int64, generator=generator).tolist()].tolist()
-        yield from self.indice[torch.randint(high=n, size=(self.num_samples % 32,), dtype=torch.int64, generator=generator).tolist()].tolist()
+        yield from self.indice[torch.randint(high=n, size=(self._num_samples % 32,), dtype=torch.int64, generator=generator).tolist()].tolist()
 
     def __len__(self) -> int:
         return self._num_samples
