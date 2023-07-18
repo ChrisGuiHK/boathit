af20c3c69960997dc998dd8f99c4fc9d74a90242
ChrisGuiHK <200110701@stu.hit.edu.cn>
2023-07-18 21:33:06 +0800

    remove methods

--- alignment/__init__.py
+++ alignment/__init__.py
@@ -1,11 +1,7 @@
 from .DomainAdversarialLoss import DomainAdversarialLoss
 from .ConditionalDomainAdversarialLoss import ConditionalDomainAdversarialLoss
-from .WeightAdversarialLoss import WeightAdversarialLoss
-from .WeightAdversarialLoss import get_partial_classes_weight
 
 __all__ = [
     DomainAdversarialLoss,
     ConditionalDomainAdversarialLoss,
-    WeightAdversarialLoss,
-    get_partial_classes_weight,
 ]
